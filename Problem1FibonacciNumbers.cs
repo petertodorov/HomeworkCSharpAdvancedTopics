439db24958eeb4ba859fcbf662ffcfe9db6f7c18
Peter Todorov <petodorov@gmail.com>
2016-08-25 14:07:54 +0300

    Add files via upload
    Fibonacci Nth number method

---
+++ Problem1FibonacciNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Problem1FibonacciNumbers
+{
+    static void Fib(int n)
+    {
+        int first = 0;
+        int second = 1;
+        for (int i = 0; i < n; i++)
+        {
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+        Console.WriteLine(second);
+    }
+    static void Main()
+    {
+        while (true)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Fib(n): ");
+            Fib(n);
+        }
+    }
+}
+
