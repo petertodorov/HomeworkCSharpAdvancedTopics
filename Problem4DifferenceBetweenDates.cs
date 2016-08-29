aba8bdb829b9b602574cc3d12b6512217ed9661a
Peter Todorov <petodorov@gmail.com>
2016-08-29 16:09:56 +0300

    Add files via upload

---
+++ Problem4DifferenceBetweenDates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+class Problem4DifferenceBetweenDates
+{
+    static double NumberOfDays(DateTime first, DateTime second)
+    {
+        TimeSpan difference = second - first;
+        int result = (int)difference.TotalDays;
+        return result;
+    }
+    static void Main()
+    {
+        string format = "dd.mm.yyyy";
+        string firstInput = Console.ReadLine();
+        DateTime firstDate = DateTime.ParseExact(firstInput,format,CultureInfo.CurrentCulture);
+        string secondInput = Console.ReadLine();
+        DateTime secondDate = DateTime.ParseExact(secondInput, format, CultureInfo.CurrentCulture);
+        int days = (int)NumberOfDays(firstDate, secondDate);
+        Console.WriteLine(days);
+    }
+    
+}
