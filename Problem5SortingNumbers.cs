6126a97be3b94209eeecc8875995b81a29d916a5
Peter Todorov <petodorov@gmail.com>
2016-08-30 13:59:53 +0300

    Add files via upload

---
+++ Problem5SortingNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Problem5SortingNumbers
+{
+
+    static void SortNumber(int n)
+    {
+        List<int> sequence = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            int temp = int.Parse(Console.ReadLine());
+            sequence.Add(temp);
+            sequence.Sort();
+        }
+        for (int i = 0; i < sequence.Count; i++)
+        {
+            Console.WriteLine(sequence[i]);
+        }
+    }
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        SortNumber(n);
+    }
+}
+
