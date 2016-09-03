e1c5ab47292e36afd2ae5d9f3241f0903e106067
Peter Todorov <petodorov@gmail.com>
2016-09-03 13:08:07 +0300

    Add files via upload

---
+++ Problem7MatrixOfPalindromes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem7MatrixOfPalindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = int.Parse(Console.ReadLine()); int c = int.Parse(Console.ReadLine());
+            for (int row = 0; row < r; row++)
+            {
+                for (int col = 0; col < c; col++)
+                {
+                    string a = Convert.ToString((char)('a' + row));
+                    string b = Convert.ToString((char)('a' + row + col));
+                    string d = Convert.ToString((char)('a' + row) + " ");
+                    Console.Write(Join(a, b, d));
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static string Join(string a, string b, string d)
+        {
+            return String.Join("", a, b, d);
+        }
+    }
+}
