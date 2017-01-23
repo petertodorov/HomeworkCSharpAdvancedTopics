4b8cd25dd34cd09fce978ecfd81d0aebd54d720c
Peter Todorov <petodorov@gmail.com>
2017-01-23 14:59:08 +0200

    Update Problem1FibonacciNumbers.cs

--- Problem1FibonacciNumbers.cs
+++ Problem1FibonacciNumbers.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-class Problem1FibonacciNumbers
+class ProblemOneFibonacciNumbers
 {
     static void Fib(int n)
     {
